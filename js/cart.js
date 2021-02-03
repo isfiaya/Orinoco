4547c4cd4cda18c152ae5a77c70973174bf9fc41
aymen isfiaya <aymen.isfiaya@gmail.com>
2021-02-03 12:01:48 +0100

    add price total

--- js/cart.js
+++ js/cart.js
@@ -9,14 +9,36 @@ for (let i = 0; i < cartArray.length; i++) {
     let nameCell = document.createElement('td');
     let lenseCell = document.createElement('td');
     let priceCell = document.createElement('td');
+    let btnRemove = document.createElement('td');
 
     nameCell.innerHTML = cartArray[i].name;
     lenseCell.innerHTML = cartArray[i].lenses;
-    priceCell.innerHTML = cartArray[i].price;
+    priceCell.innerHTML = cartArray[i].price + ' $';
+    btnRemove.innerHTML = `<button class="btn-del" id='remove'>X</button>`;
 
-    tr.append(nameCell, lenseCell, priceCell);
+    tr.append(nameCell, lenseCell, priceCell, btnRemove);
 
     cartItemsWrapper.appendChild(tr);
 }
+// // btn Remove Item
+// let remove = document.getElementById('remove')
+// remove.addEventListener('click', () => {
+//     let i = this.parentNode.parentNode.rowIndex;
+//     cartArray.splice(i, 1);
+
+// });
+
+
+// Total price 
+let total = document.getElementById('total');
+let totalPrice = 0;
+for (let i = 0; i < cartArray.length; i++) {
+    totalPrice += cartArray[i].price;
+}
+total.innerHTML = totalPrice + ' $';
+
+
+
+
 
 console.log(cartArray);
